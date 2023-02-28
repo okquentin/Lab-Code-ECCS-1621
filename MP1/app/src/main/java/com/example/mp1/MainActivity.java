87158d3d4f19d5e1c17fd5d1da82ad9d2d43dc51
okquentin <qosterhage@outlook.com>
2023-02-27 22:30:07 -0500

    MP1 Code Complete

--- MP1/app/src/main/java/com/example/mp1/MainActivity.java
+++ MP1/app/src/main/java/com/example/mp1/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainActivity extends AppCompatActivity {
 
     RandomMooValue rmv = new RandomMooValue();
@@ -21,53 +23,55 @@ public class MainActivity extends AppCompatActivity {
         // Variables
         final Button randomButton = findViewById(R.id.random);
         final Button guessButton = findViewById(R.id.guess);
-        final EditText secretValue = findViewById(R.id.editTextSecretValue);
         final EditText guessValue = findViewById(R.id.editTextGuessValue);
-        final TextView bigCount = findViewById(R.id.bigMooCount);
-        final TextView littleCount = findViewById(R.id.littleMooCount);
+        final TextView mooText = findViewById(R.id.mooText);
         final TextView correct = findViewById(R.id.correctGuess);
+        final TextView turnNum = findViewById(R.id.turnNum);
+        final int[] turn = {0};
 
         // Actions to be performed once the "RANDOM" button is pressed
         randomButton.setOnClickListener(v -> {
             final String[] secretVal = {""};
             rmv.setSecretValue();
             secretVal[0] = rmv.getSecretValue();
-            secretValue.setText(secretVal[0]);
         }); // end of setOnClickListener
 
         // Actions to be performed once the "GUESS!" button is pressed
         guessButton.setOnClickListener(v -> {
 
-            // Temp variables for error checking
-            final String[] secretVal = {""};
-            final boolean[] validValue = {false};
-
-            // Method call to set secret value
-            rmv.setSecretValue(secretValue.getText().toString());
-            secretVal[0] = rmv.getSecretValue();
-            secretValue.setText(secretVal[0]);
-
-            // Check to make sure player inputs a 4 character guess
+            // Check to make sure player inputs a 4 character guess, if the guess is valid then the turn count is incremented
             String playerGuess = guessValue.getText().toString();
-            if (playerGuess.length() != 4){guessValue.setText("Error");}
+            if (playerGuess.length() == 4){turn[0]++;}
+            else{guessValue.setText("Error");}
 
-            // Calls both "moo" count functions, displays results
+            // Calls both "moo" count functions
             int MOO = rmv.getBigMooCount(guessValue.getText().toString());
             int moo = rmv.getLittleMooCount(guessValue.getText().toString());
-            String MOOString = "" + MOO;
-            String mooString = "" + moo;
 
-            for(int i = 0; i < moo; i++){
-                createViewItem(new String[]{"Fabio", "Santos", "Programador", "Natal"});
-
+            // Initializing and concatenating a string to show 'MOO!" and "moo." (based on MOO/moo count function returns)
+            String mooString = "";
+            for(int i = 0; i < MOO; i++){
+                mooString += " MOO! ";
             }
-            bigCount.setText(MOOString);
-            littleCount.setText(mooString);
+            for(int i = 0; i < moo; i++){
+                mooString += " moo. ";
+            }
+
+            // Setting the mooText TextView to the concatenated string of "MOO!"'s and/or "moo."'s
+            mooText.setText(mooString);
+
+            // Setting the current turn count to a string to be displayed in the app
+            String turnString = String.valueOf(turn[0]);
+            turnNum.setText(turnString + ":");
 
             // Calls guess correctness check method, displays status (Yes/No)
             boolean isCorrect = rmv.isCorrectGuess(guessValue.getText().toString());
-            if(isCorrect){correct.setText("Yes!");}
-            else{correct.setText("No.");}
+            if(isCorrect){
+                correct.setText("LaurieMOO!!!");
+            }
+            else if(turn[0] >=10){
+                correct.setText("Boo hoo -- no LaurieMOO.");
+            }
         }); // end of setOnClickListener
 
     } // end of onCreate
