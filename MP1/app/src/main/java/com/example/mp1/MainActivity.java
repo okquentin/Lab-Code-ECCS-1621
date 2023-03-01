476d5f951d8b93ed764684dbc92de91b1de3d369
okquentin <qosterhage@outlook.com>
2023-03-01 15:11:41 -0500

    MP1 Finished w/ Documentation

--- MP1/app/src/main/java/com/example/mp1/MainActivity.java
+++ MP1/app/src/main/java/com/example/mp1/MainActivity.java
@@ -8,7 +8,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.concurrent.atomic.AtomicInteger;
+/**
+ * Name: MainActivity
+ * Date: 2/14/23
+ * Author: Quentin Osterhage
+ * Description: The main code for the app, with calls to functions defined in custom class
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,52 +32,101 @@ public class MainActivity extends AppCompatActivity {
         final TextView mooText = findViewById(R.id.mooText);
         final TextView correct = findViewById(R.id.correctGuess);
         final TextView turnNum = findViewById(R.id.turnNum);
+        final TextView valueWas = findViewById(R.id.valueWas);
+        final TextView revealSecret = findViewById(R.id.revealSecret);
+
         final int[] turn = {0};
+        final int[] game = {0};
 
-        // Actions to be performed once the "RANDOM" button is pressed
+        // Actions to be performed once the "RANDOM" button is pressed.
         randomButton.setOnClickListener(v -> {
-            final String[] secretVal = {""};
+
+            // Calls the setSecretValue method and sets the turn to zero.
             rmv.setSecretValue();
-            secretVal[0] = rmv.getSecretValue();
-        }); // end of setOnClickListener
+            turn[0] = 0;
 
-        // Actions to be performed once the "GUESS!" button is pressed
-        guessButton.setOnClickListener(v -> {
-
-            // Check to make sure player inputs a 4 character guess, if the guess is valid then the turn count is incremented
-            String playerGuess = guessValue.getText().toString();
-            if (playerGuess.length() == 4){turn[0]++;}
-            else{guessValue.setText("Error");}
-
-            // Calls both "moo" count functions
-            int MOO = rmv.getBigMooCount(guessValue.getText().toString());
-            int moo = rmv.getLittleMooCount(guessValue.getText().toString());
-
-            // Initializing and concatenating a string to show 'MOO!" and "moo." (based on MOO/moo count function returns)
-            String mooString = "";
-            for(int i = 0; i < MOO; i++){
-                mooString += " MOO! ";
-            }
-            for(int i = 0; i < moo; i++){
-                mooString += " moo. ";
-            }
-
-            // Setting the mooText TextView to the concatenated string of "MOO!"'s and/or "moo."'s
-            mooText.setText(mooString);
-
-            // Setting the current turn count to a string to be displayed in the app
+            // Display the turn as zero, prompt for a new guess
             String turnString = String.valueOf(turn[0]);
             turnNum.setText(turnString + ":");
+            guessValue.setText("Guess Here");
 
-            // Calls guess correctness check method, displays status (Yes/No)
-            boolean isCorrect = rmv.isCorrectGuess(guessValue.getText().toString());
-            if(isCorrect){
-                correct.setText("LaurieMOO!!!");
+        }); // end of setOnClickListener.
+
+        // Actions to be performed once the "GUESS!" button is pressed.
+        guessButton.setOnClickListener(v -> {
+            String secretVal = rmv.getSecretValue();
+            String playerGuess = guessValue.getText().toString();
+            // Check to make sure player inputs a 4 character guess, if the guess is valid then the turn count is incremented.
+            if (playerGuess.length() == 4) {
+                    turn[0]++;
+
+                    // Calls both "moo" count methods.
+                    int MOO = rmv.getBigMooCount(guessValue.getText().toString());
+                    int moo = rmv.getLittleMooCount(guessValue.getText().toString());
+
+                    // Initializing and concatenating a string to show 'MOO!" and "moo." (based on MOO/moo count function returns).
+                    StringBuilder mooString = new StringBuilder();
+                    for (int i = 0; i < MOO; i++) {
+                        mooString.append(" MOO! ");
+                    }
+                    for (int i = 0; i < moo; i++) {
+                        mooString.append(" moo. ");
+                    }
+
+                    // Setting the mooText TextView to the concatenated string of "MOO!"'s and/or "moo."'s.
+                    mooText.setText(mooString.toString());
+
+                    // Setting the mooText TextView to "all you hear are cowbells" in the case of no moo's.
+                    if (moo == 0 && MOO == 0) mooText.setText("All you hear are cowbells...");
+
+                    // Setting the current turn count to a string to be displayed in the app.
+                    String turnString = String.valueOf(turn[0]);
+                    turnNum.setText(turnString + ":");
+
+                    // Calls guess correctness check method, displays results with custom messages.
+                    boolean isCorrect = rmv.isCorrectGuess(guessValue.getText().toString());
+                    if (isCorrect) {
+                        correct.setText("LaurieMOO!!!");
+                        revealSecret.setText(secretVal);
+                        game[0]++;
+                        turn[0] = 0;
+
+                    } else if (turn[0] >= 10) {
+                        correct.setText("Boo hoo -- no LaurieMOO.");
+                        valueWas.setText("The secret was:");
+                        revealSecret.setText(secretVal);
+                        game[0]++;
+                        turn[0] = 0;
+                    }
+
+                    // Check to terminate the Game
+                    if(game[0] > 0){
+                        mooText.setText("Please generate a new value");
+                        guessValue.setText("Please generate a new value");
+                    }
+                }
+                else {
+                    // Displays an error when the length of an input guess != 4
+                    guessValue.setText("Error");
+                    mooText.setText("Please enter a valid guess.");
+                    if(turn[0] >= 0)
+                        turn[0]--;
+
+                    // Check to terminate the game
+                    if(game[0] > 0){
+                        mooText.setText("Game Over.");
+                        guessValue.setText("Game Over.");
+                    }
+                }
+
+            // Check to ensure that a value is generated before guessing can begin.
+            if (Integer.parseInt(rmv.getSecretValue()) == 0 && game[0] == 0) {
+                guessValue.setText("Error");
+                mooText.setText("Please generate a random value.");
+                if(turn[0] >= 0)
+                    turn[0]--;
             }
-            else if(turn[0] >=10){
-                correct.setText("Boo hoo -- no LaurieMOO.");
-            }
+
         }); // end of setOnClickListener
-
     } // end of onCreate
 } // end of MainActivity
--- MP1/app/src/main/java/com/example/mp1/RandomMooValue.java
+++ MP1/app/src/main/java/com/example/mp1/RandomMooValue.java
@@ -3,6 +3,13 @@ package com.example.mp1;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Name: RandomMooValue
+ * Date: 2/14/23
+ * Author: Quentin Osterhage
+ * Description:
+ */
+
 public class RandomMooValue {
 
     // Variables
@@ -10,13 +17,23 @@ public class RandomMooValue {
     private int min = 0000;
     private int max = 9999;
 
-    // Methods
+    /**
+     * setSecretValue
+     * Sets the private int 'secretVal' into a randomly generated integer between 0 and 9999.
+     * @return boolean result - always returns true
+     */
     public boolean setSecretValue(){
         Random rand = new Random();
         secretVal = rand.nextInt(9999);
         return true;
     }
 
+    /**
+     * setSecretValue
+     * Turns the private int 'secretVal' into a formatted string and returns it to the user, with error checking
+     * @param n String - the player input for setting the secret value manually
+     * @return boolean result - false when length of 'n' is greater than 4, else false
+     */
     public boolean setSecretValue(String n){
         if(n.length() > 4){return false;}
 
@@ -26,12 +43,23 @@ public class RandomMooValue {
             }
     }
 
+    /**
+     * getSecretValue
+     * Turns the private int 'secretVal' into a formatted string and returns it to the user
+     * @return string n - the formatted string of 'secretVal'
+     */
     public String getSecretValue(){
         String n = "" + secretVal;
         while(n.length() < 4) {n = '0'+n;}
         return n;
     }
 
+    /**
+     * getLittleMooCount
+     * Checks every element of the 'guess' string to the 'secret' value string
+     * @param guess String - the guess input by the player
+     * @return int MOO - the count of correctly guessed numbers in the right place
+     */
     public int getBigMooCount(String guess){
         int MOO = 0;
 
@@ -43,6 +71,12 @@ public class RandomMooValue {
         return MOO;
     }
 
+    /**
+     * getLittleMooCount
+     * Checks every element of the 'guess' string to the 'secret' value string
+     * @param guess string - the guess input by the player
+     * @return int moo - the count of correctly guessed numbers (not in the right place)
+     */
     public int getLittleMooCount(String guess){
         int moo = 0;
         String n = getSecretValue();
@@ -59,6 +93,12 @@ public class RandomMooValue {
         return moo;
     }
 
+    /**
+     * isCorrectGuess
+     * Checks whether the string input by the player is the same as the secret value
+     * @param guess String - the string input by the player
+     * @return boolean result - true if the strings are equal, false otherwise
+     */
     public boolean isCorrectGuess(String guess){
         String n = getSecretValue();
         if(guess.equals(n)){return true;}
@@ -66,3 +106,4 @@ public class RandomMooValue {
     }
 
 }
+
